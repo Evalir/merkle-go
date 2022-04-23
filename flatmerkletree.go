57d1b8846b93e070e84c081382b40194e6129059
Enrique Ortiz <hi@enriqueortiz.dev>
2022-04-23 00:13:11 -0400

    feat: 'flat' (as in using an array) merkle tree

---
+++ flatmerkletree.go
@@ -0,0 +1,205 @@
+package merklego
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math"
+)
+
+var (
+	ErrNilBlock             = errors.New("Block to insert cannot be nil")
+	ErrEmptyMerkleTree      = errors.New("Merkle tree cannot be empty; insert some blocks")
+	ErrTreeAlreadyFinalized = errors.New("Merkle tree already finalized")
+	ErrTreeNotFinalized     = errors.New("Merkle tree not finalized")
+)
+
+var (
+	internalNodePrefix = '\x01'
+	leafNodePrefix     = '\x00'
+)
+
+type (
+	FlatMerkleTree struct {
+		blocks    []Block
+		nodes     []TreeNode
+		root      TreeNode
+		finalized bool
+	}
+
+	TreeNode []byte
+	Block    []byte
+)
+
+func (t TreeNode) Bytes() []byte {
+	return t
+}
+
+func (b Block) Bytes() []byte {
+	return b
+}
+
+func NewMerkleTree(blocks ...Block) *FlatMerkleTree {
+	return &FlatMerkleTree{
+		blocks:    blocks,
+		finalized: false,
+	}
+}
+
+func (mt *FlatMerkleTree) String() (s string) {
+	if rh, err := mt.RootHash(); err == nil {
+		s = fmt.Sprintf("0x%s", hex.EncodeToString(rh))
+	}
+
+	return
+}
+
+func (mt *FlatMerkleTree) RootHash() ([]byte, error) {
+	if !mt.finalized {
+		return nil, fmt.Errorf("invalid root hash: %v", ErrTreeNotFinalized)
+	}
+
+	return copyNode(mt.root).Bytes(), nil
+}
+
+func (mt *FlatMerkleTree) Insert(block Block) error {
+	if block == nil {
+		return ErrNilBlock
+	}
+
+	if mt.finalized {
+		return ErrTreeAlreadyFinalized
+	}
+
+	if mt.blocks == nil {
+		mt.blocks = []Block{}
+	}
+
+	mt.blocks = append(mt.blocks, block)
+	return nil
+}
+
+func (mt *FlatMerkleTree) Proof(block Block) ([]TreeNode, error) {
+	if block == nil {
+		return nil, ErrNilBlock
+	}
+
+	if !mt.finalized {
+		return nil, ErrTreeNotFinalized
+	}
+
+	idx, err := mt.findLeaf(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeIdx := idx
+	k := 0
+	proof := make([]TreeNode, int(math.Log2(float64(len(mt.nodes)))))
+
+	for nodeIdx > 0 {
+		if nodeIdx%2 == 0 {
+			proof[k] = copyNode(mt.nodes[nodeIdx-1])
+		} else {
+			proof[k] = copyNode(mt.nodes[nodeIdx+1])
+		}
+		k++
+
+		nodeIdx = (nodeIdx - 1) / 2
+	}
+
+	// Proof was requested for a block on the second to last level
+	// So remove the empty last proof chunk
+	if proof[len(proof)-1] == nil {
+		return proof[:len(proof)-1], nil
+	}
+
+	return proof, nil
+}
+
+func (mt *FlatMerkleTree) Finalize() error {
+	if len(mt.blocks) == 0 {
+		return fmt.Errorf("Failed to finalize: %s", ErrEmptyMerkleTree)
+	}
+
+	if mt.finalized {
+		return ErrTreeAlreadyFinalized
+	}
+
+	if len(mt.blocks)%2 != 0 {
+		mt.blocks = append(mt.blocks, mt.blocks[len(mt.blocks)-1])
+	}
+
+	// A full binary tree composed from N items has 2 * N - 1 nodes.
+	mt.nodes = make([]TreeNode, 2*len(mt.blocks)-1)
+
+	// Set the leaf nodes to be in the last N array slots.
+	// The merkle tree array will then have the first 2 * N - (N + 1) slots
+	// with intermediate nodes, with 0 being the root.
+	j := len(mt.nodes) - len(mt.blocks)
+	for _, b := range mt.blocks {
+		mt.nodes[j] = hashNode(b, false)
+		j++
+	}
+
+	mt.root = mt.finalize(0)
+	mt.finalized = true
+
+	return nil
+}
+
+func (mt *FlatMerkleTree) finalize(idx int) TreeNode {
+	if !mt.hasChild(idx) {
+		return mt.nodes[idx]
+	}
+
+	left := mt.finalize(2*idx + 1)
+	right := mt.finalize(2*idx + 2)
+
+	mt.nodes[idx] = hashNode(append(left, right...), true)
+
+	return mt.nodes[idx]
+}
+
+func (mt *FlatMerkleTree) findLeaf(block Block) (int, error) {
+	if block == nil {
+		return -1, ErrNilBlock
+	}
+
+	for i := 0; i < len(mt.blocks); i++ {
+		if bytes.Equal(mt.blocks[i].Bytes(), block.Bytes()) {
+			return len(mt.nodes) - len(mt.blocks) + i, nil
+		}
+	}
+
+	return -1, fmt.Errorf("block does not exist: %v", hex.EncodeToString(block))
+}
+
+func (mt *FlatMerkleTree) hasChild(idx int) bool {
+	n := len(mt.nodes)
+	l := 2*idx + 1
+	r := 2*idx + 2
+
+	return l < n || r < n
+}
+
+func hashNode(data []byte, internal bool) TreeNode {
+	raw := make(TreeNode, len(data)+1)
+
+	if internal {
+		raw[0] = byte(internalNodePrefix)
+	}
+
+	copy(raw[1:], data)
+	sum := sha256.Sum256(raw)
+
+	return TreeNode(sum[:])
+}
+
+func copyNode(node TreeNode) TreeNode {
+	cpy := make(TreeNode, len(node))
+	copy(cpy, node)
+	return cpy
+}
---
+++ flatmerkletree_test.go
@@ -0,0 +1,267 @@
+package merklego
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func newTestMerkleTree() (*FlatMerkleTree, []Block) {
+	dataSize := 1000000
+	chunkSize := 10
+	chunks := dataSize / chunkSize
+	blocks := make([]Block, chunks)
+
+	for i := 0; i < chunks; i++ {
+		block := make(Block, chunkSize)
+		rand.Read(block)
+
+		blocks[i] = block
+	}
+
+	mt := NewMerkleTree(blocks...)
+	if err := mt.Finalize(); err != nil {
+		panic(err)
+	}
+
+	return mt, blocks
+}
+
+func TestInsert(t *testing.T) {
+	testCases := []struct {
+		input  Block
+		blocks []Block
+	}{
+		{nil, nil},
+		{[]byte("Hello, World!"), []Block{[]byte("Hello, World!")}},
+		{[]byte("Merkle"), []Block{[]byte("Hello, World!"), []byte("Merkle")}},
+		{[]byte("Crypto"), []Block{[]byte("Hello, World!"), []byte("Merkle"), []byte("Crypto")}},
+	}
+
+	mt := NewMerkleTree()
+
+	for i, tc := range testCases {
+		mt.Insert(tc.input)
+
+		require.Equal(t, tc.blocks, mt.blocks, fmt.Sprintf("test case #%d", i))
+	}
+}
+
+func TestRootHash(t *testing.T) {
+	testCases := []struct {
+		blocks       []Block
+		expectedRoot string
+		expectedErr  bool
+	}{
+		{
+			expectedErr: true,
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+			},
+			expectedRoot: "526885312f344b1ecf858295f8ccb0205d5a9e34f99eddf899726750183c4d4b",
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+				Block("blockC"),
+				Block("blockD"),
+			},
+			expectedRoot: "cfd8b30f6bd15f8f7f4efd80528a57c74b85bf6c3beabfcf409b11e84041e573",
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+				Block("blockC"),
+				Block("blockD"),
+				Block("blockE"),
+				Block("blockF"),
+				Block("blockG"),
+				Block("blockH"),
+			},
+			expectedRoot: "1e405b87167acaa710a77783bbc02558bacab62cf682fb1b8cf0a249a5167ad6",
+		},
+	}
+
+	for i, tc := range testCases {
+		mt := NewMerkleTree(tc.blocks...)
+
+		if len(tc.blocks) > 0 {
+			mt.Finalize()
+		}
+
+		root, err := mt.RootHash()
+
+		if tc.expectedErr {
+			require.Error(t, err, fmt.Sprintf("expected error: test case #%d", i))
+		} else {
+			expectedRoot, _ := hex.DecodeString(tc.expectedRoot)
+
+			require.NoError(t, err, fmt.Sprintf("unexpected error: test case #%d", i))
+			require.Equal(t, expectedRoot, root, fmt.Sprintf("unexpected root: test case #%d", i))
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	testCases := []struct {
+		blocks      []Block
+		expectedStr string
+	}{
+		{
+			expectedStr: "",
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+			},
+			expectedStr: "0x526885312f344b1ecf858295f8ccb0205d5a9e34f99eddf899726750183c4d4b",
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+				Block("blockC"),
+				Block("blockD"),
+				Block("blockE"),
+			},
+			expectedStr: "0x8f95978f1b0dd9d3d792cc82e5b21c7c32927c7fe7c8d477be030ab1654bbc93",
+		},
+		{
+			blocks: []Block{
+				Block("blockA"),
+				Block("blockB"),
+				Block("blockC"),
+				Block("blockD"),
+				Block("blockE"),
+				Block("blockF"),
+				Block("blockG"),
+				Block("blockH"),
+			},
+			expectedStr: "0x1e405b87167acaa710a77783bbc02558bacab62cf682fb1b8cf0a249a5167ad6",
+		},
+	}
+
+	for i, tc := range testCases {
+		mt := NewMerkleTree(tc.blocks...)
+
+		if len(tc.blocks) > 0 {
+			mt.Finalize()
+		}
+
+		require.Equal(t, tc.expectedStr, mt.String(), fmt.Sprintf("unexpected value: test case #%d", i))
+	}
+}
+
+func TestProof(t *testing.T) {
+	dataSet1 := []Block{
+		Block("blockA"),
+		Block("blockB"),
+		Block("blockC"),
+		Block("blockD"),
+		Block("blockE"),
+		Block("blockF"),
+		Block("blockG"),
+		Block("blockH"),
+	}
+	dataSet2 := []Block{
+		Block("blockA"),
+		Block("blockB"),
+		Block("blockC"),
+		Block("blockD"),
+		Block("blockE"),
+	}
+
+	testCases := []struct {
+		blocks        []Block
+		proofBlock    Block
+		expectedErr   bool
+		expectedProof []string
+	}{
+		{
+			expectedErr: true,
+		},
+		{
+			blocks:      dataSet1,
+			expectedErr: false,
+			proofBlock:  Block("blockA"),
+			expectedProof: []string{
+				"631E1AF9330CDFA88E9EB39ACE2431F5F471B93EAB8E7C085B4B40F2A5F637D7",
+				"9577C5848D134240A957225DD68A3D697C7D937592380C653DFE184F50DD8482",
+				"1E5347BF6618F0C97D2838DBC3758A9805AEF13383FB6C12ABFF31500AD66ABD",
+			},
+		},
+		{
+			blocks:      dataSet1,
+			expectedErr: false,
+			proofBlock:  Block("blockF"),
+			expectedProof: []string{
+				"5E6F4831C72462B47E9594F04DC58822FD3AB0A050452C97119E1EC017FAADF2",
+				"1212F9BB2DB7915781AFCF9E104892DB1191878C402E9C8AC4954CE9F5A0295D",
+				"CFD8B30F6BD15F8F7F4EFD80528A57C74B85BF6C3BEABFCF409B11E84041E573",
+			},
+		},
+		{
+			blocks:      dataSet2,
+			expectedErr: false,
+			proofBlock:  Block("blockA"),
+			expectedProof: []string{
+				"631E1AF9330CDFA88E9EB39ACE2431F5F471B93EAB8E7C085B4B40F2A5F637D7",
+				"E1270856BC57D48181A2946A66121663C3FAA0BBD8B6743B079CD0C5D87AD3E2",
+			},
+		},
+		{
+			blocks:      dataSet2,
+			expectedErr: false,
+			proofBlock:  Block("blockE"),
+			expectedProof: []string{
+				"5E6F4831C72462B47E9594F04DC58822FD3AB0A050452C97119E1EC017FAADF2",
+				"9577C5848D134240A957225DD68A3D697C7D937592380C653DFE184F50DD8482",
+				"526885312F344B1ECF858295F8CCB0205D5A9E34F99EDDF899726750183C4D4B",
+			},
+		},
+		{
+			blocks:      dataSet1,
+			expectedErr: true,
+			proofBlock:  Block("blockZ"),
+		},
+		{
+			blocks:      dataSet2,
+			expectedErr: true,
+			proofBlock:  Block("blockZ"),
+		},
+	}
+
+	for i, tc := range testCases {
+		mt := NewMerkleTree(tc.blocks...)
+		mt.Finalize()
+
+		proof, err := mt.Proof(tc.proofBlock)
+
+		if tc.expectedErr {
+			require.Error(t, err, fmt.Sprintf("expected error: test case #%d", i))
+		} else {
+			require.NoError(t, err, fmt.Sprintf("unexpected error: test case #%d", i))
+			require.Equal(t, len(tc.expectedProof), len(proof), fmt.Sprintf("unexpected proof length: test case #%d", i))
+
+			for j, exChunk := range tc.expectedProof {
+				chunk := fmt.Sprintf("%X", proof[j])
+
+				require.Equal(t, exChunk, chunk, fmt.Sprintf("invalid proof: test case #%d", i))
+			}
+		}
+	}
+}
